dbe6789d7389ac6475c249980739cb8480d58b31
Francesco Vigliaturo <viglia90@gmail.com>
2023-03-04 19:11:34 +0100

    Adds args parser

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,28 @@ use parser::visitor::{GraphGenerator, PrettyPrinter, Visitor};
 use parser::Parser;
 use scanner::Scanner;
 
+use clap::{arg, Command};
+
+fn get_args_parser() -> Command {
+    Command::new("jlox")
+    .version("1.0")
+    .about("jlox repl and interpreter")
+    .args(&[
+        arg!(-s --syntaxtree "generate svg with the syntaxt tree"),
+        arg!([script] "an optional script file to interpret. If this is missing a repl will be launched instead"),
+    ])
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() > 2 {
-        println!("Usage: jlox-rs [script]");
-        std::process::exit(64)
-    } else if args.len() == 2 {
-        run_file(&args[1]);
-    } else {
-        run_prompt()
+    let arg_matches = get_args_parser().get_matches();
+    let syntax_tree = arg_matches.get_flag("syntaxtree");
+    match arg_matches.get_one::<String>("script") {
+        Some(script) => run_file(script, syntax_tree),
+        None => run_prompt(syntax_tree),
     }
 }
 
-fn run_prompt() {
+fn run_prompt(syntax_tree: bool) {
     let mut input: String = String::new();
     let stdin = std::io::stdin();
     loop {
@@ -39,20 +48,34 @@ fn run_prompt() {
 
                 let mut parser = Parser::new(tokens);
                 let expr_tree = parser.parse();
+                if syntax_tree {
+                    let graph_generator = GraphGenerator::new();
+                    match graph_generator.generate_tree(&expr_tree) {
+                        Err(error) => println!("Error trying to generate the graph: {}", error),
+                        _ => {}
+                    }
+                }
             }
             Err(err) => println!("Cannot read command line input: {}", err),
         }
     }
 }
 
-fn run_file(path: &String) {
+fn run_file(path: &String, syntax_tree: bool) {
     // Open the file in read-only mode.
     match std::fs::read_to_string(path) {
         Ok(script) => {
             let mut scanner = Scanner::new(&script);
             let tokens = scanner.scan_tokens();
-            for token in tokens {
-                println!("{}", token);
+
+            let mut parser = Parser::new(tokens);
+            let expr_tree = parser.parse();
+            if syntax_tree {
+                let graph_generator = GraphGenerator::new();
+                match graph_generator.generate_tree(&expr_tree) {
+                    Err(error) => println!("Error trying to generate the graph: {}", error),
+                    _ => {}
+                }
             }
         }
         Err(err) => {
